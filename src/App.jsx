ad06eba4d7960d037abb47c0a70b4a80327756c4
Kavyansh <kd.kavyansh2003@gmail.com>
2024-06-29 07:03:25 +0530

    Added file cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header/Header"
 import { useState } from "react"
 import './App.css'
 import Sidebar from "./components/Sidebar/Sidebar"
-
+import FilesView from "./components/FilesView/FilesView"
 function App() {
   const [user, setUser] = useState({
     displayName : "kvyns",
@@ -12,10 +12,14 @@ function App() {
     photoURL: "https://images.pexels.com/photos/21345905/pexels-photo-21345905/free-photo-of-elegant-woman-leaning-on-chair.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   })
   return (
-    <>
-      <Header userPhoto={user.photoURL}/>
+    <div className="App">
+        <Header userPhoto={user.photoURL}/>
+      <div className="app__main">
       <Sidebar/>
-    </>
+      <FilesView/>
+      </div>
+      
+    </div>
   )
 }
 
--- src/components/FilesView/FilesView.jsx
+++ src/components/FilesView/FilesView.jsx
@@ -1,23 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { db } from '../../firebase';
+import FileItem from './FileItem';
+import '../../styles/filesview.css';
+import FileCard from './FileCard';
 
-function FilesView() {
-  return (
-    <div className='fileView'>
-        <div className="fileView__row">
-            {/* file cards */}
-        </div>
-        <div className="filesView__titles">
-            <div className="filesView__titles--left">
-                <p>Name</p>
-            </div>
-            <div className='fileView__titles--right'>
-                <p>Last modified</p>
-                <p>File size</p>
-            </div>
-        </div>
-        {/* file items */}
-    </div>
-  )
-}
+const FilesView = () => {
+    const [files, setFiles] = useState([]);
 
-export default FilesView
+    useEffect(() => {
+        const unsubscribe = onSnapshot(collection(db, 'myFiles'), (snapshot) => {
+            setFiles(snapshot.docs.map(doc => ({
+                id: doc.id,
+                item: doc.data()
+            })));
+        });
+
+        // Cleanup the subscription
+        return () => unsubscribe();
+    }, []);
+
+    console.log(files);
+
+    return (
+        <div className='fileView'>
+            <div className="fileView__row">
+                {
+                    files.slice(0, 5).map(({ id, item }) => (
+                        <FileCard key={id} name={item.caption} /> // Added key for list rendering
+                    ))
+                }
+            </div>
+            <div className="fileView__titles">
+                <div className="fileView__titles--left">
+                    <p>Name</p>
+                </div>
+                <div className="fileView__titles--right">
+                    <p>Last modified</p>
+                    <p>File size</p>
+                </div>
+            </div>
+            {
+                files.map(({ id, item }) => (
+                    <FileItem
+                        key={id} // Added key for list rendering
+                        id={id}
+                        caption={item.caption}
+                        timestamp={item.timestamp}
+                        fileUrl={item.fileUrl}
+                        size={item.size}
+                    />
+                ))
+            }
+        </div>
+    );
+};
+
+export default FilesView;
