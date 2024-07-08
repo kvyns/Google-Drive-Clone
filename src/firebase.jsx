d4c62ad2f2e156836d036eae99473a5a37d784be
Kavyansh <kd.kavyansh2003@gmail.com>
2024-07-08 06:25:34 +0530

    Removed Firebase credentials

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -6,14 +6,7 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAqDnPa6uqTLNm8KIMmk8gr94Z7dh4SbPc",
-  authDomain: "drive-clone-acadf.firebaseapp.com",
-  projectId: "drive-clone-acadf",
-  storageBucket: "drive-clone-acadf.appspot.com",
-  messagingSenderId: "1954943189",
-  appId: "1:1954943189:web:7f792300997ec39cce125c"
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 // Initialize Firebase
